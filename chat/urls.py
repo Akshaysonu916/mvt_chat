fb6302b0a5c2be6198e5a5f9650504974e737056
Akshay_Aneesh <120236337+Akshaysonu916@users.noreply.github.com>
2025-04-30 12:39:58 +0530

    first commit

---
+++ chat/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views 
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('chat/<str:room_name>/', views.chat_room, name='chat'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', views.user_logout, name='logout'),
+]
